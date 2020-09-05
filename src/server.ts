bc3a519b0e148ea88b6efe0e60720af03d8856a6
Andreas Aduschkin <delrem32@gmail.com>
2020-09-05 16:28:29 +0200

    server configs fix

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,9 @@ import * as Files from "./api/files";
 import * as Conversation from "./api/conversations";
 import * as Friends from "./api/friends";
 import * as Cards from "./api/cards";
+import * as TrelloColumnOrder from "./api/trello-column-order";
+import * as TrelloColumns from "./api/trello-columns";
+import * as TrelloTasks from "./api/trello-tasks";
 import { IDatabase } from "./database";
 import { OrdersModel } from "./api/orders/orders";
 import * as Orders from "./api/orders";
@@ -63,6 +66,9 @@ export async function init(
     Friends.init(server, configs, database);
     Cards.init(server, configs, database);
     Orders.init(server, configs, database);
+    TrelloColumns.init(server, configs, database);
+    TrelloColumnOrder.init(server, configs, database);
+    TrelloTasks.init(server, configs, database);
     console.log("Routes registered sucessfully.");
 
     return server;
