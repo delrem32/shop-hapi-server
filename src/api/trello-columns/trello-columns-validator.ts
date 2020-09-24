6d4b48ed391794518324c58cbdbbfaf5e85ae40c
Andreas Aduschkin <delrem32@gmail.com>
2020-09-24 16:55:59 +0200

    fix validation

--- src/api/trello-columns/trello-columns-validator.ts
+++ src/api/trello-columns/trello-columns-validator.ts
@@ -3,7 +3,6 @@ import * as Joi from "joi";
 export const createColumnValidator = Joi.object({
   title: Joi.string(),
   taskIds: Joi.array().items(Joi.string()).allow([]),
-  columnOrderId: Joi.string().required(),
 });
 export const getColumnsByIdsValidator = Joi.object({
   ids: Joi.array().items(Joi.string()),
